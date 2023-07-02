550de6aff85e2d9235db3695f9cbac527089f7c3
hyun-jung-joo <wnguswjd0418@naver.com>
2023-07-02 21:53:21 +0900

    [#21] [feat] home connect

--- diaryProject/postDiary/models.py
+++ diaryProject/postDiary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model) :
+    titleDate = models.DateTimeField(verbose_name="작성일", auto_now_add=True)
+    body = models.TextField(verbose_name="내용", default="")
+    image = models.ImageField(verbose_name='이미지', blank=True, null=True, upload_to='post-image')
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self) :
+        return self.title
+    
+class Comment(models.Model) :
+    comment = models.CharField(max_length=200)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self) :
+        return self.comment
--- diaryProject/postDiary/views.py
+++ diaryProject/postDiary/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request): 
+	return render(request, "main.html") # index.html 렌더링
+
+def mainlist(request) : 
+    return render(request, "mainList.html")
