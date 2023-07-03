8d30116652092c41dae40af03da97808966f63d7
hyun-jung-joo <wnguswjd0418@naver.com>
2023-07-03 20:06:21 +0900

    [#29][feat] comment 기초작업

--- diaryProject/postDiary/forms.py
+++ diaryProject/postDiary/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
             # 어떤 필드를 입력 받을지 써주기
         fields = ['content', 'image'] 
+
+class CommentForm(forms.ModelForm) :
+    class Meta :
+        model = Comment
+        fields = ['comment']
